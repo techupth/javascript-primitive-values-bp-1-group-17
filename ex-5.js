af0108f04c0fabca1641d81ee784234b2a61d885
mosigaaa <mosiga2008@gmail.com>
2023-09-16 12:07:32 +0700

    "feat: Complete Assignment1 "

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,6 @@
 //Exercise #5: Shopping and Discount
-
 // Start coding here
+let totalProductValue = 27000;
+let Discount = 0.25;
+let DiscoutedProduct = totalProductValue - (totalProductValue * Discount);
+console.log(DiscoutedProduct);
--- ex-6.js
+++ ex-6.js
@@ -1,6 +1,5 @@
 // Exercise #6: Displaying message
 
 let totalParts = 13000;
-
-let concatSentence;
-let literalSentence;
+let concatSentence = "Total: " + totalParts + " Machine parts";
+let literalSentence = `Total: ${totalParts} Machine parts`;
